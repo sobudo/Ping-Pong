eaaff0d8a35408ffc7c3be04dbbbc315c46c1ee5
sobudo <sobudo@gmail.com>
2018-02-23 12:01:15 +0300

    Added return functions fro numbers 3, 5 and 3 and 5

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,7 +1,23 @@
 // business logic
 var numbergame = function(number) {
-  if ((number % 2 === 0) || (number % 3 == 0) || (number % 4 === 0)) {
-    return true;
+  if ((number % 3 === 0)) {
+    return Ping;
+  } else {
+    return false;
+  }
+};
+
+var numbergame = function(number) {
+  if ((number % 5 == 0)) {
+    return Pong;
+  } else {
+    return false;
+  }
+};
+
+var numbergame = function(number) {
+  if ((number % 3 === 0) && (number % 5 == 0)) {
+    return Ping pong;
   } else {
     return false;
   }
